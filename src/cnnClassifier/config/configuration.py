85a3ddbc4a50b280c1e389e47d7ac545c98dd45a
Tyson Barreto <Tyson.Barreto@aat.org.uk>
2024-09-09 14:39:21 +0100

    st01.ipynb added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -10,7 +10,7 @@ class ConfigurationManager:
         config_filepath = CONFIG_FILE_PATH,
         params_filepath = PARAMS_FILE_PATH):
         self.config = read_yaml(config_filepath)
-        #self.params = read_yaml(params_filepath)
+        self.params = read_yaml(params_filepath)
         create_directories([self.config.artifacts_root])
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
